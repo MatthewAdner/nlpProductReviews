104cafdb13f2d4caaf50316b134c6ec2c91031dd
MatthewAdner <matthew77001@gmail.com>
2021-06-26 03:32:54 -0400

    added comments to test.py

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
+# imports libraries
 from typing import Text
 from nltk.util import pr
 from textblob import TextBlob
+
+# prints out polarity and subjectivity subjectivity is how subjective/objective it is, polarity is positive or negative
 testimonial = TextBlob("Text blob is a library.")
 print(testimonial.sentiment)
+
+# tests a negative statement for polarity (postive/negative sentiment)
 negTesti = TextBlob("I hate text blob and it is terrible! Who would ever use something so terrible as text blob?!")
 print(f"negTesti is {negTesti.sentiment.polarity}")
+
+# tests a positive statement for polarity
 postesti = TextBlob("Text blob is great! I love it!")
 print(f"posTesti is {postesti.sentiment.polarity}")
