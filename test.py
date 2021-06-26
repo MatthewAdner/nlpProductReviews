a13bf8ccb15552f95121ad9a740f4a2d5adf4589
MatthewAdner <matthew77001@gmail.com>
2021-06-26 03:20:39 -0400

    did some more stuff with TextBlob in test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+from typing import Text
 from nltk.util import pr
 from textblob import TextBlob
-testomonial = TextBlob("Text blob is a library.")
-print(testomonial.sentiment)
+testimonial = TextBlob("Text blob is a library.")
+print(testimonial.sentiment)
+negTesti = TextBlob("I hate text blob and it is terrible! Who would ever use something so terrible as text blob?!")
+print(f"negTesti is {negTesti.sentiment.polarity}")
+postesti = TextBlob("Text blob is great! I love it!")
+print(f"posTesti is {postesti.sentiment.polarity}")
